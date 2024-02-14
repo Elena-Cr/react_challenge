76e66472d727788b42f268555c53dea550c77122
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-14 15:29:43 +0100

    completed challenge

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar imageAvatar="public\avatar.jpg" />
       <div className="data">
-        <Intro />
+        <Intro
+          name="Elena Cristescu"
+          bio="Learning JavaScript and React in order to become a front-end developer. Reacent graduate of Bussiness, Economics and IT at KEA."
+        />
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
@@ -15,6 +18,40 @@ function App() {
       </div>
     </div>
   );
+}
+function Avatar(props) {
+  return (
+    <div className="avatar">
+      <img src={props.imageAvatar} alt="avatar" />
+    </div>
+  );
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.bio}</h3>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" />
+      <Skill skill="JavaScript" />
+      <Skill skill="Web Development" />
+      <Skill skill="Git & GitHub" />
+      <Skill skill="React" />
+      <Skill skill="SQL" />
+      <Skill skill="MongoDB" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return <div className="skill">{props.skill}</div>;
 }
 
 const rootElement = document.getElementById("root");
